c5562d8a2c0d658f0467889d02efcbacd6517bc1
Eric Baril <baril27@gmail.com>
2018-05-10 15:08:35 -0700

    Add basic contract doctype

---
+++ rentals/rentals/doctype/contract/test_contract.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2018, Eric Baril and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+import frappe
+import unittest
+
+class TestContract(unittest.TestCase):
+	pass
