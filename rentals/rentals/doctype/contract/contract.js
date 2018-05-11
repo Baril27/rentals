8578052fb8fa51732bb388a0e3edcf844435dce2
Eric Baril <baril27@gmail.com>
2018-05-11 15:30:56 -0700

    Add custom pickup and delivery buttons to contract

--- rentals/rentals/doctype/contract/contract.js
+++ rentals/rentals/doctype/contract/contract.js
@@ -2,7 +2,22 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Contract', {
-	refresh: function(frm) {
+  refresh: function(frm) {
+    // Delivery Ticket
+    frm.add_custom_button(__('Delivery'), function() {
+      frappe.model.open_mapped_doc({
+        method: "rentals.rentals.doctype.contract.contract.make_delivery_note",
+        frm: frm
+      })
+    }, __("Make"));
 
-	}
+    // Pickup Ticket
+    frm.add_custom_button(__('Pickup'), function() {
+      frappe.model.open_mapped_doc({
+        method: "rentals.rentals.doctype.contract.contract.make_pickup_note",
+        frm: frm
+      })
+    }, __("Make"));
+    frm.page.set_inner_btn_group_as_primary(__("Make"));
+  }
 });
--- rentals/rentals/doctype/contract/contract.py
+++ rentals/rentals/doctype/contract/contract.py
@@ -5,6 +5,31 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from frappe.model.mapper import get_mapped_doc
 
-class Contract(Document):
-	pass
+class Contract(Document): pass
+
+@frappe.whitelist()
+def make_delivery_note(source_name, target_doc=None):
+  target_doc = get_mapped_doc("Contract", source_name, {
+    "Contract": {
+      "doctype": "Delivery Note",
+      "validation": {
+        "docstatus": ["=", 1]
+      }
+    }
+  }, target_doc, None)
+
+  return target_doc
+
+def make_pickup_note(source_name, target_doc=None):
+  target_doc = get_mapped_doc("Contract", source_name, {
+    "Contract": {
+      "doctype": "Delivery Note",
+      "validation": {
+        "docstatus": ["=", 1]
+      }
+    }
+  }, target_doc, None)
+
+  return target_doc
